79aa1dc830859e0338e1d63e8100fad7f83c937b
santhoshpsk <santhoshpsk@outlook.com>
2024-01-04 19:27:57 +0530

    Create config_vars.py
    This is the config file that contains db details. Db hostname is set based on the service name from the docker-compose file

---
+++ eeazycrm/config_vars.py
@@ -0,0 +1,27 @@
+
+# DEVELOPMENT KEY
+DEV_SECRET_KEY = 'your_secret_key'
+
+# TESTING KEY
+TEST_SECRET_KEY = 'test_secret_key'
+
+# PRODUCTION KEY
+SECRET_KEY = 'your_production_key'
+
+# DEVELOPMENT DATABASE SETTINGS
+DEV_DB_HOST = 'db'
+DEV_DB_USER = 'eeazycrm'
+DEV_DB_PASS = 'eeazycrm'
+DEV_DB_NAME = 'eeazycrm'
+
+# TEST DATABASE SETTINGS
+TEST_DB_HOST = 'db'
+TEST_DB_USER = 'eeazycrm'
+TEST_DB_PASS = 'eeazycrm'
+TEST_DB_NAME = 'eeazycrm'
+
+# PRODUCTION DATABASE SETTINGS (Default)
+DB_HOST = 'db'
+DB_USER = 'eeazycrm'
+DB_PASS = 'eeazycrm'
+DB_NAME = 'eeazycrm'
